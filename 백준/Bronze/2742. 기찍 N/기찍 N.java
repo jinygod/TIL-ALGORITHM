aaa966a32eafb171710fd2a0ac32f79d1dd01757
jinygod <103230840+jinygod@users.noreply.github.com>
2022-06-15 21:51:30 +0900

    [Bronze III] Title: 기찍 N, Time: 1004 ms, Memory: 32816 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		
+	    Scanner in = new Scanner(System.in);
+		
+	    int num = in.nextInt();
+	    for(int i = 1; i <= num; num--) {
+	    	System.out.println(num);
+	    	
+	    }
+	   
+
+	}
+
+}
