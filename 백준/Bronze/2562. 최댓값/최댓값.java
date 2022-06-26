65b932e4b4569762a2a94de8d13015cfce462927
suma93 <103230840+jinygod@users.noreply.github.com>
2022-06-26 23:30:22 +0900

    [Bronze III] Title: 최댓값, Time: 232 ms, Memory: 18400 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = { sc.nextInt(), sc.nextInt(), sc.nextInt(),
+				sc.nextInt(), sc.nextInt(), sc.nextInt(),
+				sc.nextInt(), sc.nextInt(), sc.nextInt() };
+		sc.close();
+		
+		int count = 0;
+		int max = 0;
+		int index = 0;
+        
+		for(int value : arr) {
+			count++;
+            
+			if(value > max) {
+				max = value;
+				index = count;
+			}
+		}
+		System.out.print(max + "\n" + index);
+		
+	}
+}
