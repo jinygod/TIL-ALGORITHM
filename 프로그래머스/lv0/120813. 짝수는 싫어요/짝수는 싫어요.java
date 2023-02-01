3af0ae1ad888aeb93c1364f010240afe7d7716bd
전성진 <103230840+jinygod@users.noreply.github.com>
2023-02-01 03:24:02 -0800

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n/2) + (n%2)];
+        
+            for(int i = 0; i < n/2 + n%2; i++){
+    
+                  
+                answer[i] += 2* i + 1 ;
+            }
+        
+        
+        return answer;
+    }
+}
