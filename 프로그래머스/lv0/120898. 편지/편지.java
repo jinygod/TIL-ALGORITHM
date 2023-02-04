dc82b0e9ec5bdbfe4877d7c2e3cc47b7f21c71f9
전성진 <103230840+jinygod@users.noreply.github.com>
2023-02-04 00:41:02 -0800

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        
+        char[] arr = message.toCharArray();
+        
+        return arr.length * 2 ;
+    }
+}
