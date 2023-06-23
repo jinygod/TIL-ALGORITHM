7371448cc77a40e39ec53337af4b3f613cc5178b
전성진 <103230840+jinygod@users.noreply.github.com>
2023-06-23 13:29:31 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 14952 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,32 +1,42 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.io.IOException;
- 
- 
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int N = Integer.parseInt(br.readLine()); //입력 개수
-		
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		
-		int max = -1;
-		double sum = 0.0;
-		
-		for (int i = 0; i < N; i++) {
-			int value = Integer.parseInt(st.nextToken());
-			
-			if(value > max) {
-				max = value;
-			}
-			
-			sum += value;
-		}
-	
-		System.out.println(((sum / max) * 100.0) / N);
-		
-	}
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        try {
+            int N = Integer.parseInt(br.readLine());
+            double[] N_list = new double[N];
+            
+            String[] input = br.readLine().split(" ");
+            for (int i = 0; i < N_list.length; i++) {
+                N_list[i] = Double.parseDouble(input[i]);
+            }
+            
+            double max_value = N_list[0];
+            
+            for (int i = 1; i < N_list.length; i++) {
+                if (N_list[i] > max_value) {
+                    max_value = N_list[i];
+                }
+            }
+            
+            double[] new_score = new double[N];
+            
+            double sum = 0.0;
+            
+            for (int i = 0; i < new_score.length; i++) {
+                new_score[i] = N_list[i] / max_value * 100;
+                sum += new_score[i];
+            }
+            
+            double avg = sum / N;
+            
+            System.out.println(avg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
