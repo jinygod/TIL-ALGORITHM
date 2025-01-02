08d820039847dde14e27d121194d7ad3e1e6cc38
전성진 <103230840+jinygod@users.noreply.github.com>
2025-01-02 16:37:44 +0900

    [Bronze III] Title: 최댓값, Time: 176 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,26 +1,24 @@
-import java.util.Scanner;
- 
-public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int[] arr = { sc.nextInt(), sc.nextInt(), sc.nextInt(),
-				sc.nextInt(), sc.nextInt(), sc.nextInt(),
-				sc.nextInt(), sc.nextInt(), sc.nextInt() };
-		sc.close();
-		
-		int count = 0;
-		int max = 0;
-		int index = 0;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         
-		for(int value : arr) {
-			count++;
-            
-			if(value > max) {
-				max = value;
-				index = count;
-			}
-		}
-		System.out.print(max + "\n" + index);
-		
-	}
+        int[] arr = new int[9];
+        int max = 0;
+        int cnt = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+            if(arr[i] > max){
+                max = arr[i];
+                cnt= i +1;
+            }
+        }
+        
+        System.out.println(max);
+        System.out.println(cnt);
+        
+       
+    }
 }
