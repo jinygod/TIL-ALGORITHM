0c8707fb6a61664ebe86e0e4dff7edaa418b7f82
전성진 <103230840+jinygod@users.noreply.github.com>
2025-01-05 00:27:44 -0800

    [Silver IV] Title: ATM, Time: 116 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,41 +1,37 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
+import java.util.*;
+import java.io.*;
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int N = Integer.parseInt(br.readLine());
- 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
- 
-		// 입력의 범위는 1 ~ 1000이므로 1001개의 index를 둔다.
-		int[] arr = new int[1001];
- 
-		// Counting Sort 
-		while (N-- > 0) {
-			arr[Integer.parseInt(st.nextToken())]++;
-		}
- 
-		int prev = 0;	// 이전까지의 대기시간 누적합
-		int sum = 0;	// 사람별 대기시간 총합
-		
-		for (int i = 0; i < 1001; i++) {
-			
-			// 해당 i index가 0이 될 때 까지 반복 
-			while (arr[i]-- > 0) {
-				// 이전까지의 대기시간과 현재 사람이 걸리는 시간을 더해준다.
-				sum += (i + prev);
-				
-				// 이전까지의 누적합에 현재 걸리는 시간을 더해준다.
-				prev += i;
-			}
-		}
-		System.out.println(sum);
-	}
- 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] minute = new int[N];
+
+        // 입력받기
+        String[] inputs = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            minute[i] = Integer.parseInt(inputs[i]);
+        }
+
+        // 오름차순 정렬
+        Arrays.sort(minute);
+
+        int total = 0;     // 현재까지의 누적 시간
+        int realTotal = 0; // 전체 대기 시간의 합
+
+        // 누적 합 계산
+        for (int i = 0; i < N; i++) {
+            total += minute[i];
+            realTotal += total;
+        }
+
+        // 결과 출력
+        bw.write(String.valueOf(realTotal) + "\n"); // 정수를 문자열로 변환
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
 }
