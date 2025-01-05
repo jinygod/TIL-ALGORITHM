63538818abcd4ff4444ec0c9f56b88c742c0d601
전성진 <103230840+jinygod@users.noreply.github.com>
2025-01-05 01:30:10 -0800

    [Silver II] Title: 잃어버린 괄호, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,36 +1,33 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
- 
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int sum = Integer.MAX_VALUE;	// 초기 상태 여부 확인을 위한 값으로 설정 
-		String[] subtraction = br.readLine().split("-");
-		
- 
-		for(int i = 0; i < subtraction.length; i++) {
-			int temp = 0;
- 
-			// 뺄셈으로 나뉜 토큰을 덧셈으로 분리하여 해당 토큰들을 더한다.
-			String[] addition = subtraction[i].split("\\+");
-			
-			// 덧셈으로 나뉜 토큰들을 모두 더한다. 
-			for(int j = 0; j < addition.length; j++) {
-				temp += Integer.parseInt(addition[j]);
-			}
-			
-			// 첫 번째토큰인 경우 temp값이 첫 번째 수가 됨
-			if (sum == Integer.MAX_VALUE) {
-				sum = temp;
-			} else {
-				sum -= temp;
-			}
-		}
-		System.out.println(sum);
-	}
- 
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = Integer.MAX_VALUE; // 초기값 설정
+        String[] subtraction = br.readLine().replace(" ", "").split("-"); // 공백 제거 후 '-'로 분리
+
+        for (int i = 0; i < subtraction.length; i++) {
+            int temp = 0;
+
+            // "+"로 나누기
+            String[] addition = subtraction[i].split("\\+");
+            for (String part : addition) {
+                if (!part.isEmpty()) { // 빈 문자열 방지
+                    temp += Integer.parseInt(part);
+                }
+            }
+
+            // 첫 번째 값 처리
+            if (sum == Integer.MAX_VALUE) {
+                sum = temp;
+            } else {
+                sum -= temp;
+            }
+        }
+        System.out.println(sum);
+    }
 }
