b96478b0515abb8a763c460ca200f37bec8625ee
전성진 <103230840+jinygod@users.noreply.github.com>
2025-01-07 18:20:12 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,37 +1,28 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-
-	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		int[] coin = new int[N];
-		
-		for(int i = 0; i < N; i++) {
-			coin[i] = Integer.parseInt(br.readLine());
-		}
-		
-		int count = 0;
-
-		for(int i = N - 1; i >= 0; i--) {
-
-			// 현재 동전의 가치가 K보다 작거나 같아야지 구성가능하다.
-			if(coin[i] <= K) {
-				// 현재 가치의 동전으로 구성할 수 있는 개수를 더해준다.
-				count += (K / coin[i]);
-				K = K % coin[i];
-			}
-		}
-		System.out.println(count);
-	}
-
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+        
+        int[] coin = new int[N];
+        
+        for(int i = 0; i < coin.length; i++){
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+        
+        int count = 0;
+        for(int i = N -1; i >= 0; i--){
+            if(K / coin[i] > 0){
+                count += K / coin[i];
+                K %= coin[i];
+            }
+        }
+        System.out.print(count);
+        br.close();
+    }
 }
