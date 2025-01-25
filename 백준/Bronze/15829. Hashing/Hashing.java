c35ae559f14527bbea589e6a048c50d0474e88b0
전성진 <103230840+jinygod@users.noreply.github.com>
2025-01-25 15:54:44 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14176 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,20 +1,25 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int L = Integer.parseInt(br.readLine());
+        long L = Long.parseLong(br.readLine()); // 문자열 길이
+        String word = br.readLine(); // 입력 문자열
         
-        String word = br.readLine();
+        long answer = 0;
+        long power = 1; // 31^i 값을 저장
         
-        int answer = 0;
+        final long mod = 1234567891; // 주어진 mod 값
         
-        for(int i = 0; i < word.length(); i++){
-            answer += (word.charAt(i)-96) * (Math.pow(31,i));
+        for (int i = 0; i < word.length(); i++) {
+            // (현재 문자 값 * power) % mod
+            answer = (answer + (word.charAt(i) - 'a' + 1) * power) % mod;
+            // 다음 power 값 계산
+            power = (power * 31) % mod;
         }
         
-        System.out.print(answer);
+        System.out.println(answer);
     }
 }
